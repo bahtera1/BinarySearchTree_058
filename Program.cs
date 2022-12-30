570dd8780a2370c1baaf7d7a49efa1acdf072601
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2022-12-30 17:49:29 +0700

    create menu

--- Program.cs
+++ Program.cs
@@ -37,7 +37,7 @@ namespace BinarySearchTree_058
         {
             Node tmp, parent = null, currentNode = null;
             Search(element, ref parent, ref currentNode);
-            if(currentNode != null) //check if the node to be inserted already inserted or not
+            if (currentNode != null) //check if the node to be inserted already inserted or not
             {
                 Console.WriteLine("Duplicate words not allowed");
                 return;
@@ -46,11 +46,11 @@ namespace BinarySearchTree_058
             else //if the spesiefied is not present
             {
                 tmp = new Node(element, null, null); //create a Node
-                if(parent == null)  //if the tree is empty
+                if (parent == null)  //if the tree is empty
                 {
-                    ROOT - tmp;
+                    ROOT = tmp;
                 }
-                else if(string.Compare(element,parent.info)<0)
+                else if (string.Compare(element, parent.info) < 0)
                 {
                     parent.leftchild = tmp;
                 }
@@ -60,12 +60,12 @@ namespace BinarySearchTree_058
                 }
             }
         }
-        public void Search(string element,ref Node parent,ref Node currentNode)
+        public void Search(string element, ref Node parent, ref Node currentNode)
         {
             //this function searches the currentnode of the specified node as well as the current node of its parent
             currentNode = ROOT;
             parent = null;
-            while((currentNode!= null)&&(currentNode.info !=element))
+            while ((currentNode != null) && (currentNode.info != element))
             {
                 parent = currentNode;
                 if (String.Compare(element, currentNode.info) < 0)
@@ -77,15 +77,15 @@ namespace BinarySearchTree_058
 
         public void inorder(Node ptr)
         {
-            if(ROOT== null)
+            if (ROOT == null)
             {
                 Console.WriteLine("Tree is Empty");
                 return;
             }
-            if(ptr != null)
+            if (ptr != null)
             {
                 inorder(ptr.leftchild);
-                Console.Write(ptr.info+" ");
+                Console.Write(ptr.info + " ");
                 inorder(ptr.rightchild);
             }
         }
@@ -96,49 +96,54 @@ namespace BinarySearchTree_058
                 Console.WriteLine("Tree Is empty");
                 return;
             }
-            if(ptr!= null)
+            if (ptr != null)
             {
-                Console.WriteLine(ptr.info+"");
+                Console.WriteLine(ptr.info + "");
                 preorder(ptr.leftchild);
                 preorder(ptr.rightchild);
             }
         }
         public void postorder(Node ptr) //performs the psotorder traversal of the tree
         {
-            if(ROOT== null)
+            if (ROOT == null)
             {
                 Console.WriteLine("Tree is empty");
                 return;
             }
-            if(ptr != null)
+            if (ptr != null)
             {
                 postorder(ptr.leftchild);
                 postorder(ptr.rightchild);
-                Console.WriteLine(ptr.info+ "");
+                Console.WriteLine(ptr.info + "");
             }
         }
-    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    internal class Program
-    {
-     
         static void Main(string[] args)
         {
+            BinaryTree x = new BinaryTree();
+            while(true)
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. Implement insert operation");
+                Console.WriteLine("2. Implement inorder traversal");
+                Console.WriteLine("3. implement preorder traversal");
+                Console.WriteLine("4. Implement postorder traversak");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("\nEnter Your Choice(1-5)");
+                char ch = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine();
+                switch(ch)
+                {
+                    case '1':
+                        {
+                            Console.WriteLine("Enter a word: ");
+                            string word = Console.WriteLine();
+                            x.insert(word);
+                        }
+                        break
+                    case 
+                }
+            }
         }
     }
 }
