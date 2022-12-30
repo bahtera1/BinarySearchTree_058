a17bb693995e2f453ebd01e3d7ca75ee6456adea
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2022-12-30 17:15:02 +0700

    create class node

--- Program.cs
+++ Program.cs
@@ -6,8 +6,40 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree_058
 {
+    class Node
+    {
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+
+        //konstruktor untuk class node
+
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     internal class Program
     {
+     
         static void Main(string[] args)
         {
         }
